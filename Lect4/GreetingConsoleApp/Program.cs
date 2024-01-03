64af260a34b6e54c36c9b8cb12b06e2212627e83
Muzamil Dev <Muzamil Dev@DESKTOP-RRVVA3N>
2024-01-03 23:37:26 +0500

    greeding app practiced

--- Lect4/GreetingConsoleApp/Program.cs
+++ Lect4/GreetingConsoleApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//In this application , we will welcome the user to our application and then we will ask them for their first name. Finally, we will Greet them by name
+
+
+// Greeting The Client to The Application
+Console.WriteLine(" Welcome To The Application ");
+Console.WriteLine(" This Applicaiton is Developed by Muzamil Mughal By Love <3");
+Console.WriteLine("---------------------------------------------------------");
+Console.WriteLine();
+
+
+// Asking Customer to Entery His / Her Name
+Console.WriteLine("Please Enter Your Name : ");
+string? FirstName = "";
+FirstName = Console.ReadLine();
+
+
+
+// Greet Customer with His or Her Name
+Console.WriteLine();
+Console.WriteLine("Hi, " + FirstName);
+Console.WriteLine("Thanks for using our Application!");
